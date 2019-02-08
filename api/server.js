1d356814f67fd43931faea46573c2180b21f5969
Jeremiah Ogutu <jogutu@pm.me>
2019-02-08 10:30:23 -0500

    Made express server

---
+++ api/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const PORT = 4000;
+const cors = require('cors');
+
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.listen(PORT, () => console.log('Server is running on Port:',PORT));
