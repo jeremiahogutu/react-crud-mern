5b641d716ce7e285f2d6877d4f344df0b4a27303
Jeremiah Ogutu <jogutu@pm.me>
2019-02-07 22:43:31 -0500

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import Create from "./components/create";
+import Edit from "./components/edit";
+import Index from "./components";
 
 class App extends Component {
     render() {
         return (
-            <div className='container'>
-                <h2>React CRUD</h2>
-            </div>
+            <Router>
+                <div className='container'>
+                    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                        <Link to={'/'} className="navbar-brand">React Crud</Link>
+                        <div className="collapse navbar-collapse" id="navbarSupprtedContent">
+                            <ul className="navbar-nav mr-auto">
+                                <li className="nav-item">
+                                    <Link to={'/'} className="nav-link">Home</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link to={'/create'} className="nav-link">Create</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link to={'/index'} className="nav-link">Index</Link>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav> <br/>
+                    <h2>React CRUD</h2>
+                    <Switch>
+                        <Route exact path='/create' component={ Create }/>
+                        <Route path='/edit/:id' component={ Edit }/>
+                        <Route path='/index' component={ Index }/>
+                    </Switch>
+                </div>
+            </Router>
+
         )
     }
 }
