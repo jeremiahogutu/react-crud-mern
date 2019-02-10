a87a1cbb61b8c25af99d20578f715060c2cedb99
Jeremiah Ogutu <jogutu@pm.me>
2019-02-09 23:40:30 -0500

    added routes to server.js

--- api/server.js
+++ api/server.js
@@ -5,6 +5,7 @@ const PORT = 4000;
 const cors = require('cors');
 const mongoose = require('mongoose');
 const config = require('./DB.js');
+const postRoute = require('./post.route');
 
 mongoose.Promise = global.Promise;
 mongoose.connect(config.DB, { useNewUrlParser: true }).then(
@@ -15,5 +16,7 @@ mongoose.connect(config.DB, { useNewUrlParser: true }).then(
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+app.use('/post', postRoute);
 
 app.listen(PORT, () => console.log('Server is running on Port:',PORT));
